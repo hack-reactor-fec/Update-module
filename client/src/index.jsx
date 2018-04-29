69b624ffd61a3db3cbff551d42e978ed650dffa0
Michael Ye <micxye@yahoo.com>
2018-04-29 13:54:45 -0700

    final s3-bucket version for proxy server integration

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,3 @@ import App from './components/App.jsx';
 window.React = React;
 window.ReactDOM = ReactDOM;
 window.Updates = App; 
-
-//ReactDOM.render(<App projectId={2}/>, document.getElementById('Updates'));
-
